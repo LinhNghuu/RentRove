f63d8bfe5e8a6959afde7ef924f02ad895896aea
LinhNgHuu <linh.nghuu3110@gmail.com>
2024-01-14 17:24:52 -0500

    AS1

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API Listening' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  console.log(`App listening at http://localhost:${port}`);
+});
